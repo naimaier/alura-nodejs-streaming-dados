5cc350bb037211a6733d74b98ffd29a3d40a0cc8
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-16 10:28:54 -0300

    Replaced buffer with stream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,12 +1,8 @@
 const fs = require('fs')
 
-fs.readFile('./assets/cachorro.jpeg', (erro, buffer) => {
-    console.log('Imagem no buffer')
+fs.createReadStream('./assets/cachorro.jpeg')
+    .pipe(fs.createWriteStream('./assets/cachorro2.jpg'))
+    .on('finish', () => console.log('Imagem escrita com sucesso!'))
 
-    fs.writeFile('./assets/cachorro2.jpg', buffer, (erro) => {
-        console.log('Imagem escrita')
-    })
-})
-
-// Se a imagem for grande o callback pode demorar a ser chamado.
-// Como essa é uma função síncrona ela vai travar a execução do programa
+// Como seu processamento é em paralelo, não teremos um callback
+// A stream possui eventos
