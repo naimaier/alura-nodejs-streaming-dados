a8ded175f53eee9d909716ad8b04ede0e5fca429
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-17 09:13:08 -0300

    Added security check when uploading files

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -2,12 +2,22 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const tiposValidos = ['jpg', 'jpeg', 'png']
     const extensao = path.extname(caminho)
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${extensao}`
+    const tipoEhValido = tiposValidos.indexOf(extensao.substring(1)) !== -1
+    // estamos removendo o ponto da extensao antes de comparar
 
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+    if (tipoEhValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${extensao}`
+    
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = 'Tipo é inválido'
+        console.log('Erro! Tipo inválido')
+        callbackImagemCriada(erro)
+    }
 }
 
 // Como seu processamento é em paralelo, não teremos um callback
--- models/animais.js
+++ models/animais.js
@@ -5,18 +5,22 @@ class Animal {
     adiciona(animal, resposta) {
         const sql = `INSERT INTO Animais SET ?`
 
-        uploadDeArquivo(animal.imagem, animal.nome, (novoCaminho) => {
+        uploadDeArquivo(animal.imagem, animal.nome, (erro, novoCaminho) => {
 
-            const novoAnimal = {nome: animal.nome, imagem: novoCaminho}
-
-            conexao.query(sql, novoAnimal, (erro, resultados) => {
-                if (erro) {
-                    console.log(erro)
-                    resposta.status(400).json(erro)
-                } else {
-                    resposta.status(201).json(novoAnimal)
-                }
-            })
+            if (erro) {
+                resposta.status(400).json({erro})
+            } else {
+                const novoAnimal = {nome: animal.nome, imagem: novoCaminho}
+    
+                conexao.query(sql, novoAnimal, (erro, resultados) => {
+                    if (erro) {
+                        console.log(erro)
+                        resposta.status(400).json(erro)
+                    } else {
+                        resposta.status(201).json(novoAnimal)
+                    }
+                })
+            }
         })
 
     }
